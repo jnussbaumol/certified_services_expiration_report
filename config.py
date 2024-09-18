9c6cf08fda428964f0092ac5a195f7356f9dcc97
Joshua Nussbaum <145164917+jnussbaumol@users.noreply.github.com>
2024-09-18 14:26:23 -0500

    update config

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 """
 Configuration file, containing variables used throughout the rest of the script for easy editng and maintenance.
 Files are lists of relative filepaths to the source files
-Networks are lists of IP ranges written in CIDR notation 
+Networks are lists of IP ranges written in CIDR notation
 Port ranges are lists of strings containing the target port ranges in the format start-end
 Ports are lists of integers
 Hosts are dictionaries with key-value pairs of domain(string):ip(string)
 """
 
-# source_files = 'test.csv'
+source_files = "test.csv"
 
 # allowlist
 # target_networks = [
@@ -16,12 +16,12 @@ Hosts are dictionaries with key-value pairs of domain(string):ip(string)
 # target_port_ranges = [
 #     '0-127',
 # ]
-# target_ports = [
-#     0,
-# ] 
+target_ports = [
+    443,
+]
 # target_hosts = {
 #     'domainhere.tld':'127.0.0.1',
-# } 
+# }
 
 # blocklist
 # excluded_networks = [
